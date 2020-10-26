f7354486588d5b6b72c5a9f3d4c98ebc1b8e9282
CZF <279154451@qq.com>
2020-10-26 20:32:24 +0800

    热修复框架

---
+++ app/src/main/java/com/single/code/shotfix/BugTest.java
@@ -0,0 +1,13 @@
+package com.single.code.shotfix;
+
+/**
+ * 创建时间：2020/10/25
+ * 创建人：singleCode
+ * 功能描述：
+ **/
+public class BugTest {
+    public static void test(){
+        System.out.println("bug");
+        throw new NullPointerException("哈哈出错啦");
+    }
+}
---
+++ app/src/main/java/com/single/code/shotfix/UIApplication.java
@@ -0,0 +1,26 @@
+package com.single.code.shotfix;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.single.patch.hotfix.EnjoyFix;
+
+import java.io.File;
+
+/**
+ * 创建时间：2020/10/12
+ * 创建人：singleCode
+ * 功能描述：
+ **/
+public class UIApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        EnjoyFix.installPatch(this, new File("/sdcard/patch.jar"));
+    }
+}
